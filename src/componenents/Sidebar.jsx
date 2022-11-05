2b0f71fd8f909dc046ec06ddd5cafbbc10bfb454
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-05 11:23:15 -0300

    feat: :construction: commence Sidebar component

---
+++ src/componenents/Sidebar.jsx
@@ -0,0 +1,22 @@
+function Sidebar () {
+  return (
+  <nav className="bg-orange-700 h-screen w-80 p-14 flex flex-col justify-between">
+    <div className="flex flex-col gap-8 justify-center items-center	">
+      <img 
+        src="https://d335luupugsy2.cloudfront.net/cms/files/107693/1639661491/$s20z34nynz"
+        alt="Orange Juice logo"
+        className="w-40"
+      />
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </div>
+    <div>Sair</div>
+  </nav>
+  )
+}
+
+export default Sidebar
