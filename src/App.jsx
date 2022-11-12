b6b340fcc085e7808488a45ef0cabc2fe4f07051
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-12 18:13:35 -0300

    style: :listick: adjust pd top of all page components at breakpoint 960px

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <div>
         <Sidebar setView={setView} view={view} />
       </div>
-      <main className="pt-10 pb-36 min-[960px]:py-20 px-8 min-[960px]:px-16 overflow-y-auto w-full">
+      <main className="pt-14 pb-36 min-[960px]:py-20 px-8 min-[960px]:px-16 overflow-y-auto w-full">
         <Animate id={view}>{views[view]}</Animate>
       </main>
     </div>
